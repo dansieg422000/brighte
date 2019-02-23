c21b115eb4d8c3630eb62c65e92031959e587d48
Ronald <ronald_olay@yahoo.com>
2019-02-23 21:17:26 +1100

    1st commit on Brighte repository

---
+++ src/Controller/BrighteController.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Controller;
+
+
+use Symfony\Component\HttpFoundation\Response;
+
+class BrighteController
+{
+    public function delivery()
+    {
+        echo 'test here';
+        exit;
+    }
+
+}
